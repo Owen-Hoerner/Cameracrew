970795afe951da7b6609e569e0abfc9158dd45f3
Owen-Hoerner <56082459+Owen-Hoerner@users.noreply.github.com>
2021-02-09 09:09:28 -0600

    Add files via upload

---
+++ final working program/usbcam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mjpg_streamer -i "input_uvc.so -d /dev/video0 -f 10 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 8080" &
+mjpg_streamer -i "input_uvc.so -d /dev/video2 -f 10 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 8090" &
+mjpg_streamer -i "input_uvc.so -d /dev/video4 -f 10 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 9000" &
+mjpg_streamer -i "input_uvc.so -d /dev/video6 -f 10 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 9010" &
