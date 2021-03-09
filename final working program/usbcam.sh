d2fad99794a6f8507e6ff045683100c38e5f2fba
Owen-Hoerner <56082459+Owen-Hoerner@users.noreply.github.com>
2021-03-09 08:39:59 -0600

    Update usbcam.sh

--- final working program/usbcam.sh
+++ final working program/usbcam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mjpg_streamer -i "input_uvc.so -d /dev/video0 -f 10 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 8080" &
-mjpg_streamer -i "input_uvc.so -d /dev/video2 -f 10 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 8090" &
-mjpg_streamer -i "input_uvc.so -d /dev/video4 -f 10 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 9000" &
-mjpg_streamer -i "input_uvc.so -d /dev/video6 -f 10 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 9010" &
+mjpg_streamer -i "input_uvc.so -d /dev/video0 -f 5 -r 480x360 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 8080" &
+mjpg_streamer -i "input_uvc.so -d /dev/video2 -f 5 -r 480x360 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 8090" &
+mjpg_streamer -i "input_uvc.so -d /dev/video4 -f 5 -r 480x360 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 9000" &
+mjpg_streamer -i "input_uvc.so -d /dev/video6 -f 5 -r 480x360 -n" -o "output_http.so -w /usr/src/mjpg-streamer-experimental/www -p 9010" &
